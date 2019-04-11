3c9a3ad2b071059f5e4f94a0ab9ff78b88ba20d6
Manolo Bevia <manolo.bevia@gmail.com>
2019-04-11 18:03:40 +0000

    Add new header to interior content.

--- wp-content/themes/culu/header.php
+++ wp-content/themes/culu/header.php
@@ -156,16 +156,7 @@
 
 	</style>
 
-	<section class="
-
-		<?php
-			if ( is_front_page() ) {
-				echo "hero__content";
-			} else {
-				echo "hero__content interior-pages";
-			}
-		 ?>
-	" aria-label="Hero header">
+	<section class="<?php if ( is_front_page() ) { echo 'hero__content'; } else { echo 'hero__content interior-pages'; } ?>" aria-label="Hero header">
 
 		<div class="all-libraries">
 			<a href="https://www.library.cornell.edu/"><span class="fa fa-arrow-left" aria-hidden="true"></span> ALL LIBRARIES</a> <span> | <a href="https://www.library.cornell.edu/libraries">Hours</a></span>  <span>| <a href="https://www.library.cornell.edu/ask/email">Ask a Librarian</span></a>

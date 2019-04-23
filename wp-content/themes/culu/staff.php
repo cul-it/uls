5a31897e85fd88f6d8c6d123ec3a734c41732afd
Manolo Bevia <manolo.bevia@cornell.edu>
2019-04-23 17:15:24 +0000

    Create staff profile template. Remove non used favicons.

--- wp-content/themes/culu/staff.php
+++ wp-content/themes/culu/staff.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single staff posts
+ * The template for displaying single staff post
  * Template Name: Staff
  * Template Post Type: post
  *
@@ -20,16 +20,55 @@ get_header();
 
 		the_post();
 
-		get_template_part( 'template-parts/content', get_post_type() );
+		?>
+
+		<section class="staff-profile" aria-label="Staff profile">
+
+
+			  <img class="staff-photo" src="<?php the_field('photo');?>" alt="">
+
+
+
+				<h2><?php echo the_field('first_name'); echo the_field('last_name'); ?></h2>
+				<h3><?php echo the_field('title'); ?></h3>
+
+
+				<p><a href="mailto:<?php echo the_field('email');?>"><?php echo the_field('email');?></a></p>
+
+
+				<p>Phone: <?php the_field('phone');?></p>
+
+				<p><a href="#" class="btn-graphic"><?php echo the_field('consultation'); ?>Book a Consultation</a></p>
+
+			</div>
+
+		</section>
+
+		<?php
 
 		the_post_navigation();
 
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) :
-			//comments_template();
-		endif;
+
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'culu' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+
 
 	endwhile; // End of the loop.
+
 	?>
 
 </main><!-- #main -->

0c15585c09e0a9aae0911be75b3e54d838ff3624
Manuel Bevia <manolo.bevia@gmail.com>
2019-06-19 19:20:17 +0000

    Add weekly grid view widget to theme.

--- wp-content/themes/culu/header.php
+++ wp-content/themes/culu/header.php
@@ -203,7 +203,7 @@
 					<span class="libcal-status-now"><?php echo do_shortcode('[libcal_status_now]') ?></span>
 					<span class="libcal-hours-today"> <?php echo do_shortcode('[libcal_hours_today]') ?> </span>
 
-					- <a class="full-hours" href="<?php echo $full_hours_label ?>">Full Hours</a> /
+					- <a class="full-hours" href="/full-hours">Full Hours</a> /
 				</div>
 
 					<ul class="header-contact">
